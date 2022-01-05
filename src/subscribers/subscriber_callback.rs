7f1f5f5c8e695c573a1e78c23fb1ec215ec48a0e
Andrey <amigin@gmail.com>
2022-01-05 17:44:17 +0200

    Made Some Changes

--- src/subscribers/subscriber_callback.rs
+++ src/subscribers/subscriber_callback.rs
@@ -1,12 +1,6 @@
 use async_trait::async_trait;
-use my_service_bus_shared::MessageId;
 
 use super::MySbDeliveryPackage;
-pub struct MySbMessage {
-    pub id: MessageId,
-    pub attempt_no: i32,
-    pub content: Vec<u8>,
-}
 
 #[async_trait]
 pub trait SubscriberCallback {
