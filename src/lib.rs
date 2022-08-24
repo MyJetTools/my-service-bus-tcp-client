3fc231fff4d18042b2fd1f955c9265745c335496
amigin <amigin@gmail.com>
2022-08-24 13:45:07 +0300

    PublishError is exposed

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod publishers;
 pub mod subscribers;
 mod tcp;
 
-pub use publishers::{MySbPublisherData, MySbPublishers};
+pub use publishers::{MySbPublisherData, MySbPublishers, PublishError};
 pub use tcp::{MessageToPublish, MyServiceBusClient};
