d39de05582b001001b3022a048b9a1aa1cce0a67
amigin <amigin@gmail.com>
2022-09-24 22:06:57 +0200

    MyServiceBusSettings exposed

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,4 @@ pub mod subscribers;
 mod tcp;
 
 pub use publishers::{MySbPublisherData, MySbPublishers, PublishError};
-pub use tcp::{MessageToPublish, MyServiceBusClient};
+pub use tcp::{MessageToPublish, MyServiceBusClient, MyServiceBusSettings};
